fed858b117f5d83680ca4ce4ab3ed98a3d80d021
1seyoung <20171631@edu.hanbat.ac.kr>
2024-07-03 11:18:57 +0900

    강한 결합을 느슨한 결합으로

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -3,7 +3,6 @@ package com.sparta.memo.controller;
 import com.sparta.memo.dto.MemoRequestDto;
 import com.sparta.memo.dto.MemoResponseDto;
 import com.sparta.memo.service.MemoService;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,10 +11,12 @@ import java.util.List;
 @RequestMapping("/api")
 public class MemoController {
 
+    //제어 흐름 MemoController -> MemoService -> MemoRepository : 강한 결합
+
     private final MemoService memoService;
 
-    public MemoController(JdbcTemplate jdbcTemplate) {
-        this.memoService = new MemoService(jdbcTemplate);
+    public MemoController(MemoService memoService) {
+        this.memoService = memoService;
     }
 
     @PostMapping("/memos")
